619fe004a2655065e8d3d2fb68cbf83d2c7b55e6
Devon Hubner <devon@hubner.org>
2021-01-18 09:57:52 -0500

    removed hard-coded cognito urls

--- utils/frontend-auth.ts
+++ utils/frontend-auth.ts
@@ -29,15 +29,7 @@ function getEnvironment(): Environment {
   }
 }
 export function getSignInUrl() {
-  const developmentUrl =
-    "https://swccgdb.auth.us-east-2.amazoncognito.com/login?client_id=4ab8m6p1nglpa4110o2jhpk17&response_type=token&scope=email+openid+profile&redirect_uri=http://localhost:3000/callback";
-  const productionUrl =
-    "https://swccgdb-production.auth.us-east-2.amazoncognito.com/login?client_id=3hganbcfgre49cutjo2acf2kv0&response_type=token&scope=email+openid+profile&redirect_uri=https://test.swccgdb-staging.com/callback";
-  if (getEnvironment() === Environment.prod) {
-    return productionUrl;
-  } else {
-    return developmentUrl;
-  }
+  return module.exports.serverRuntimeConfig.COGNITO_LOGON_URL;
 }
 export function getUserId() {
   const token = getToken();
