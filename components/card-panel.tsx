31b9baf0e24f897f121a3111f722a750067331f1
imad <1209347+hashgit@users.noreply.github.com>
2021-05-14 23:56:42 +1000

    display correct icon in edit card panel

--- components/card-panel.tsx
+++ components/card-panel.tsx
@@ -161,7 +161,7 @@ export function CardPanel({
               <div style={{ height: "3px" }}></div>
             )}
             <DeckBuilderHeader>
-              <img src="https://res.starwarsccg.org/deckdb/dark.png" style={{ height: "50px" }}></img>
+              <img src={`https://res.starwarsccg.org/deckdb/${deck.side.toLowerCase()}.png`} style={{ height: "50px" }}></img>
 
               <DeckBuilderTitle>
                 <span
